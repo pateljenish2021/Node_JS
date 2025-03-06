e716622d416d3075bcb7e617840b1be8d68a13d3
pateljenish2021 <pateljenish2021@gmail.com>
2025-03-06 20:04:03 +0530

    intial commit

--- Pr-4.Book_Store/server.js
+++ Pr-4.Book_Store/server.js
@@ -5,57 +5,61 @@ const connectDB = require("./config/dbConnection");
 const Book = require("./models/book.model");
 const Favorite = require("./models/favorite.model");
 
-const app = express();
+const server = express();
 const PORT = 3000;
 
 connectDB();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
+server.use(bodyParser.urlencoded({ extended: true }));
+server.set("view engine", "ejs");
+server.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", async (req, res) => {
+server.get("/", async (req, res) => {
   const books = await Book.find();
   res.render("index", { books });
 });
 
-app.get("/admin", async (req, res) => {
+server.get("/admin", async (req, res) => {
   const books = await Book.find();
   const book = req.query.edit ? await Book.findById(req.query.edit) : null;
   res.render("admin", { books, book });
 });
 
-app.post("/admin/add-book", async (req, res) => {
+server.post("/admin/add-book", async (req, res) => {
   await Book.create(req.body);
   res.redirect("/admin");
 });
 
-app.post("/admin/update", async (req, res) => {
+server.post("/admin/update", async (req, res) => {
   await Book.findByIdAndUpdate(req.body.id, req.body);
   res.redirect("/admin");
 });
 
-app.post("/admin/delete", async (req, res) => {
+server.post("/admin/delete", async (req, res) => {
   await Book.findByIdAndDelete(req.body.id);
   res.redirect("/admin");
 });
 
-app.get("/book", async (req, res) => {
+server.get("/book", async (req, res) => {
   const book = await Book.findById(req.query.id);
   res.render("book", { book });
 });
 
-app.get("/favorites/book", async (req, res) => {
+server.get("/favorites/book", async (req, res) => {
   const book = await Book.findById(req.query.id);
   res.render("book", { book });
 });
 
-app.get("/favorites", async (req, res) => {
+server.get("/favorites", async (req, res) => {
   const favorites = await Favorite.find();
-  res.render("fav", { books: favorites });
+  const books = await Promise.all(
+    favorites.map(async (fav) => await Book.findById(fav.bookId))
+  );
+  res.render("fav", { books });
 });
 
-app.post("/add-favorite", async (req, res) => {
+
+server.post("/add-favorite", async (req, res) => {
   const book = await Book.findById(req.body.bookId);
   if (book) {
     const alreadyFav = await Favorite.findOne({ bookId: book._id });
@@ -68,14 +72,15 @@ app.post("/add-favorite", async (req, res) => {
       });
     }
   }
-  res.redirect("/");
-});
-
-app.post("/remove-favorite", async (req, res) => {
-  await Favorite.findByIdAndDelete(req.body.id);
   res.redirect("/favorites");
 });
 
-app.listen(PORT, () =>
+server.post("/remove-favorite", async (req, res) => {
+  await Favorite.findOneAndDelete({ bookId: req.body.id });
+  res.redirect("/favorites");
+});
+
+
+server.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
 );
