2dae06b80bd586f39c9bb9c3f5bd50e86be57e8b
pateljenish2021 <pateljenish2021@gmail.com>
2025-03-19 12:26:36 +0530

    initial commit

--- Pr-5.BookMyShow/config/db.js
+++ Pr-5.BookMyShow/config/db.js
@@ -2,11 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/bookmyshow');
+        await mongoose.connect('mongodb+srv://pateljenish:patel123@odyssey.bkjon.mongodb.net/?retryWrites=true&w=majority&appName=odyssey/bookmyshow');
         console.log('MongoDB Connected');
     } catch (error) {
         console.error('MongoDB Connection Error:', error.message);
-        process.exit(1);
     }
 };
 
